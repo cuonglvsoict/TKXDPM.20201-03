e158a00d874c1b34347c66c659c5b37a1766e398
bigzenoh <bigzenoh@users.noreply.github.com>
2020-11-30 09:39:52 +0700

    test

---
+++ Programming/src/ecobikerental/viewbike/controller/ViewController.java
@@ -0,0 +1,35 @@
+package ecobikerental.viewbike.controller;
+
+import ecobikerental.viewbike.gui.MapScreen;
+import ecobikerental.viewbike.model.Bike;
+import ecobikerental.viewbike.model.Customer;
+
+public class ViewController {
+
+	
+	public boolean checkRentStatus(int cuustomerID) {
+		return true;
+	}
+	
+	public void displayInfo(int customerID) {
+		Customer customer = new Customer(customerID);
+		// đã thuê xe, hiện thông tin 1 xe
+		if (customer.getBike()!=null) {
+			displayBike(customerID);
+		} else {
+		// chưa thuê xe, hiện danh sách Dock
+			displayMap();			
+		}
+	}
+
+	private void displayMap() {
+		MapScreen.display();	
+	}
+	
+	private void displayBike(int customerID) {
+		Bike bike = new Bike(customerID);
+		bike.display();
+	}
+	
+	
+}
